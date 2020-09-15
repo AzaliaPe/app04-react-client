b6e7b63027594aac13827da8c4245cce11f419b7
Azalia Peña Hernandez <azaliaphdz@hotmail.com>
2020-09-15 13:42:08 -0700

    Publicar proyecto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'; //Las llaves son para no poner .component
-import PageTitle from './components/PageTitle';
+import TacosAdmin from './components/TacosAdmin';
 
 export default class App extends Component
 {
   render()
   {
     return(
-      <PageTitle text='El Taquito Feliz' color='pink'/>
+      <TacosAdmin/>
     );
   }
 }
--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,7 +1,12 @@
 import React, {Component, Fragment} from 'react';
 
-export default class PageTitle extends Component
-{
+export default class PageTitle extends Component{
+
+    state = 
+    {
+        //Si fontSize existe o si no ponle valor de 10 
+        fontSize: this.props.fontSize ? this.props.fontSize : 10 
+    }; 
 
     componentDidUpdate()
     {
@@ -12,11 +17,13 @@ export default class PageTitle extends Component
     {
         return (
             <Fragment>
-                <input type="number" name="" id="" value='5'/>
+                <input type="number" name="" id="" onChange={event =>{
+                    this.setState({fontSize: event.target.value});
+                }}/>
 
                 <h1 style={{
                     color: this.props.color,
-                    fontSize: '24em'
+                    fontSize: `${this.state.fontSize}em`
                 }}>{this.props.text}</h1>
             </Fragment>
         );
