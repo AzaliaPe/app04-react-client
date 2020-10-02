fd719c4ebacfba2f96374f36c11b276a1451428c
Azalia Peña Hernandez <azaliaphdz@hotmail.com>
2020-10-02 09:09:51 -0700

    Publicar proyecto

--- src/components/EditTaco.jsx
+++ src/components/EditTaco.jsx
@@ -1,48 +1,60 @@
 import React, {Component, Fragment} from 'react';
 import axios from 'axios';
 
-export default class AddTaco extends Component{
+
+export default class EditTaco extends Component{
 
     state = {
+        taco: {},
         name: '',
         quantity: 0,
         pica: 'si'
-    }
-
-    sendTaco = () =>{
-        const {name, quantity, pica} = this.state;
-        //console.log({name: name, quantity: quantity, pica: pica});
-
-        axios.post('http://localhost:5000', {name: name, quantity: quantity, pica: pica})
-        .then(response => response.data)
-        .then(data => this.props.history.push('/'));
     }
 
     catchName = event => this.setState({name: event.target.value});
     catchQuantity = event => this.setState({quantity: event.target.value});
     catchSpacyness = event => this.setState({pica: event.target.value});
 
+    componentDidMount(){
+        const tacoId = this.props.history.location.state.tacoId;
+        axios.get(`http://localhost:5000/${tacoId}`)
+        .then(reponse => reponse.data)
+        .then(taco => {
+            this.setState({taco: taco})
+            console.log(this.state.taco)
+        });
+    }
+
+    saveChanges = () => {
+        const {taco, name, quantity, pica} = this.state;
+        axios.put(`http://localhost:5000/${taco.id}`, {
+            name: name,
+            quantity: quantity,
+            pica: pica
+        });
+    }
+
     render() {
         return (
-            <Fragment>
-                <h3>Agregar taco:</h3>
+             <Fragment>
+                <h3>Editar taco:</h3>
                     <br/>
                     <div className='form-group' style={{width: '50%'}}>
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroup-sizing-default">Nombre</span>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">Nombre</span>
                             </div>
                             <input onChange={this.catchName} className='form-control' 
                             type="text" name="" id="taco-name" placeholder='eje: tu taco'
-                            aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
+                            aria-label="Default" aria-describedby="inputGroup-sizing-default" value={this.state.taco.name}/>
                         </div>
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroup-sizing-default">Cantidad</span>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">Cantidad</span>
                             </div>
                             <input onChange={this.catchQuantity} className='form-control' 
                             type="number" name="" id="taco-quantity" placeholder='eje: tu taco'
-                            aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
+                            aria-label="Default" aria-describedby="inputGroup-sizing-default" value={this.state.taco.quantity}/>
                         </div>
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
@@ -57,10 +69,12 @@ export default class AddTaco extends Component{
                         <br/>
                         <br/>
                         <div>
-                            <button onClick={this.sendTaco} className='btn btn-secondary' id="btn-post-taco">Crear taco</button>
+                            <button onClick={this.sendTaco} className='btn btn-secondary' id="btn-post-taco">Guardar</button>
                         </div>
                     </div>
             </Fragment>
         );
     }
 }
+             
+        
